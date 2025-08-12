ca5c3432e55eba76c4e6cfb75c33dd3d8959e7cf
unknown <naveenpalanisamy545@gmail.com>
2025-08-12 13:48:28 +0530

    commit

---
+++ day 6/task.js
@@ -0,0 +1,13 @@
+let ms = prompt("enter your martial status (married/unmarried): ");
+
+if(ms.toLowerCase() == "married") {
+console.log("eligible to the insurence.");
+} else if (ms.toLowerCase() == "unmarried") {
+console.log("enter the below details");
+let ms = prompt("")
+    
+}
+else{
+console.log("invail input .try agian.!!");
+    
+}
