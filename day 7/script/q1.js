525b05f9e1f2bd5f885dfd3888a2fcf8fcf4b275
unknown <naveenpalanisamy545@gmail.com>
2025-08-12 15:16:05 +0530

    commit

---
+++ day 7/script/q1.js
@@ -0,0 +1,27 @@
+/** Q1
+	 * Write a program to read the radius of a circle and calculate 
+	 * its area using the formula:
+		A = π * r²
+	
+	Problem Flow:
+	1. User inputs radius (r)
+	2. Program calculates: area = (22.0 / 7) * r * r
+	3. Output displays area with radius
+	
+	Input / Output Example:
+	Input:
+	Enter Radius of Circle: 7
+	Output:
+	Area of Circle with Radius 7.0000 is 154.0000
+*/
+
+
+ let radius = prompt("enter radius of circle:")
+ let area = (22.0/7)*radius*radius;
+ console.log(radius);
+ console.log(area);
+ 
+ 
+
+
+
---
+++ day 7/script/q2.js
@@ -0,0 +1,34 @@
+/** Q2
+	 * Write a Java program to calculate Simple Interest using the formula:
+ 		SI = (P × N × R) / 100
+ 		Where:
+		P = Principal amount
+		N = Number of years
+		R = Rate of interest
+		
+		Problem Flow:
+		User inputs:
+			Principal (P)
+			Rate (R)
+			Years (N)
+		Program calculates:
+			SI = (P × N × R) / 100
+		Output displays the Simple Interest value.
+		
+		Input / Output Example:
+		Input:
+		Enter Principal Amount: 10000  
+		Enter Rate of Interest: 5  
+		Enter No. of Years: 2
+		Output:
+		Simple Interest = 1000.00
+*/
+
+
+let p =10000;
+let n =2;
+let r =5;
+si = (p*n*r)/100;
+console.log ("simple interst:",si);
+
+
